979c530c52e4c6577ac23b6bb0ea43e229cdae82
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2025-01-14 17:59:11 +1300

    Version rollback

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['gradient-svg-generator.vercel.app'],
-  },
-}
-
-module.exports = nextConfig 
