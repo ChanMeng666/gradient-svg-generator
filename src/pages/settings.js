ec3879a2244e860ecb755b507b3839db4bfcce0b
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2025-01-14 23:33:53 +1300

    Update settings.js

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -8,7 +8,7 @@ import { MdPreview } from 'react-icons/md';
 // 定义所有模板配置
 const templateCategories = {
   basic: {
-    label: '基础模板',
+    label: 'Basic Templates',
     templates: [
       {
         name: 'sunset-gold',
@@ -49,11 +49,59 @@ const templateCategories = {
         gradientType: 'horizontal',
         animationDuration: '5s',
         description: 'Vibrant neon gradient'
+      },
+      {
+        name: 'cyber-punk',
+        label: 'Cyber Punk',
+        colors: ['ff00ff', '00ffff', 'ff00cc'],
+        gradientType: 'diagonal',
+        animationDuration: '8s',
+        description: 'Vibrant cyberpunk-inspired gradient'
+      },
+      {
+        name: 'northern-lights',
+        label: 'Northern Lights',
+        colors: ['9400d3', '4b0082', '00ff00', '00ffff'],
+        gradientType: 'vertical',
+        animationDuration: '10s',
+        description: 'Aurora borealis inspired colors'
+      },
+      {
+        name: 'cotton-candy',
+        label: 'Cotton Candy',
+        colors: ['ff99cc', 'ff99ff', '99ccff'],
+        gradientType: 'circular',
+        animationDuration: '7s',
+        description: 'Sweet pastel gradient blend'
+      },
+      {
+        name: 'midnight-galaxy',
+        label: 'Midnight Galaxy',
+        colors: ['000033', '4b0082', '800080', '000066'],
+        gradientType: 'radial',
+        animationDuration: '12s',
+        description: 'Deep space inspired colors'
+      },
+      {
+        name: 'tropical-paradise',
+        label: 'Tropical Paradise',
+        colors: ['00ff99', '33ccff', '00ffcc'],
+        gradientType: 'diagonal',
+        animationDuration: '8s',
+        description: 'Fresh tropical ocean colors'
+      },
+      {
+        name: 'desert-dusk',
+        label: 'Desert Dusk',
+        colors: ['ff9966', 'ff6699', 'cc6699'],
+        gradientType: 'horizontal',
+        animationDuration: '9s',
+        description: 'Warm desert sunset colors'
       }
     ]
   },
   pride: {
-    label: 'Pride 旗帜',
+    label: 'Pride Templates',
     templates: [
       {
         name: 'pride-rainbow',
@@ -150,6 +198,70 @@ const templateCategories = {
         gradientType: 'horizontal',
         animationDuration: '6s',
         description: 'Agender pride flag colors'
+      },
+      {
+        name: 'poly-pride',
+        label: 'Polyamory Pride',
+        colors: ['0000ff', 'ff0000', '000000'],
+        gradientType: 'horizontal',
+        animationDuration: '6s',
+        description: 'Polyamory pride flag colors'
+      },
+      {
+        name: 'demiboy-pride',
+        label: 'Demiboy Pride',
+        colors: ['7f7f7f', 'c4c4c4', '9ad9eb', 'ffffff'],
+        gradientType: 'horizontal',
+        animationDuration: '6s',
+        description: 'Demiboy pride flag colors'
+      },
+      {
+        name: 'demigirl-pride',
+        label: 'Demigirl Pride',
+        colors: ['7f7f7f', 'c4c4c4', 'ffaec9', 'ffffff'],
+        gradientType: 'horizontal',
+        animationDuration: '6s',
+        description: 'Demigirl pride flag colors'
+      },
+      {
+        name: 'omnisexual-pride',
+        label: 'Omnisexual Pride',
+        colors: ['ff9ccd', 'ff53bd', '2c2c2c', '6760ff', '9cb9ff'],
+        gradientType: 'horizontal',
+        animationDuration: '6s',
+        description: 'Omnisexual pride flag colors'
+      },
+      {
+        name: 'progress-pride',
+        label: 'Progress Pride',
+        colors: ['000000', '784F17', 'ff0000', 'ff8c00', 'ffff00', '008000', '0000ff', '4b0082'],
+        gradientType: 'diagonal',
+        animationDuration: '8s',
+        description: 'Progress pride flag with inclusive colors'
+      },
+      {
+        name: 'demiromantic-pride',
+        label: 'Demiromantic Pride',
+        colors: ['ffffff', '3da542', '000000', 'a9a9a9'],
+        gradientType: 'horizontal',
+        animationDuration: '6s',
+        description: 'Demiromantic pride flag colors'
+      },
+      {
+        name: 'demisexual-pride',
+        label: 'Demisexual Pride',
+        colors: ['ffffff', '6e0070', 'd2d2d2', '000000'],
+        gradientType: 'horizontal',
+        animationDuration: '6s',
+        description: 'Demisexual pride flag colors'
+      },
+      {
+        name: 'queer-pride',
+        label: 'Queer Pride',
+        colors: ['b57edc', 'ffffff', '4a8123', '000000'],
+        gradientType: 'vertical',
+        animationDuration: '7s',
+        description: 'Queer pride flag colors'
       }
     ]
   }
