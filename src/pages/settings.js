ed5d8f9b62799386476c2fa1260cc9346604e028
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2025-01-14 23:17:16 +1300

    Update settings.js

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -101,12 +101,127 @@ export default function Settings() {
   
   const templateCategories = {
     basic: {
-      label: 'Basic Templates',
-      templates: Object.values(basicTemplates)
+      label: '基础模板',
+      templates: [
+        {
+          name: 'sunset-gold',
+          label: 'Sunset Gold',
+          colors: ['ffd700', 'ff8c00', 'ff4500'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Warm golden sunset gradient'
+        },
+        {
+          name: 'ocean-heart',
+          label: 'Ocean Heart',
+          colors: ['00ffff', '0080ff', '0000ff'],
+          gradientType: 'vertical',
+          animationDuration: '8s',
+          description: 'Deep ocean blue gradient'
+        },
+        // ... 其他基础模板
+      ]
     },
     pride: {
-      label: 'Pride Templates',
-      templates: Object.values(prideTemplates)
+      label: 'Pride 旗帜',
+      templates: [
+        {
+          name: 'pride-rainbow',
+          label: 'Pride Rainbow',
+          colors: ['ff0000', 'ff8c00', 'ffff00', '008000', '0000ff', '4b0082'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Traditional pride rainbow flag'
+        },
+        {
+          name: 'trans-pride',
+          label: 'Trans Pride',
+          colors: ['55cdfc', 'f7a8b8', 'ffffff', 'f7a8b8', '55cdfc'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Trans pride flag colors'
+        },
+        {
+          name: 'bi-pride',
+          label: 'Bi Pride',
+          colors: ['d60270', '9b4f96', '0038a8'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Bi pride flag colors'
+        },
+        {
+          name: 'pan-pride',
+          label: 'Pan Pride',
+          colors: ['ff1b8d', 'ffd800', '00b5ff'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Pan pride flag colors'
+        },
+        {
+          name: 'nonbinary-pride',
+          label: 'Nonbinary Pride',
+          colors: ['fcf434', 'ffffff', '9c59d1', '2c2c2c'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Nonbinary pride flag colors'
+        },
+        {
+          name: 'lesbian-pride',
+          label: 'Lesbian Pride',
+          colors: ['d62900', 'ff9b55', 'ffffff', 'd461a6', 'a50062'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Modern lesbian pride flag colors'
+        },
+        {
+          name: 'genderqueer-pride',
+          label: 'Genderqueer Pride',
+          colors: ['b57edc', 'ffffff', '4a8123'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Genderqueer pride flag colors'
+        },
+        {
+          name: 'genderfluid-pride',
+          label: 'Genderfluid Pride',
+          colors: ['ff75a2', 'ffffff', 'be18d6', '000000', '333ebd'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Genderfluid pride flag colors'
+        },
+        {
+          name: 'ace-pride',
+          label: 'Ace Pride',
+          colors: ['000000', 'a3a3a3', 'ffffff', '800080'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Asexual pride flag colors'
+        },
+        {
+          name: 'aro-pride',
+          label: 'Aromantic Pride',
+          colors: ['3da542', 'a7d379', 'ffffff', 'a9a9a9', '000000'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Aromantic pride flag colors'
+        },
+        {
+          name: 'intersex-pride',
+          label: 'Intersex Pride',
+          colors: ['ffd800', '7902aa'],
+          gradientType: 'circular',
+          animationDuration: '6s',
+          description: 'Intersex pride flag colors with unique circular gradient'
+        },
+        {
+          name: 'agender-pride',
+          label: 'Agender Pride',
+          colors: ['000000', 'b9b9b9', 'ffffff', 'b8f483', 'ffffff', 'b9b9b9', '000000'],
+          gradientType: 'horizontal',
+          animationDuration: '6s',
+          description: 'Agender pride flag colors'
+        }
+      ]
     }
   };
 
